0bd50771faeff93b155c160751d616cccaeeebff
tejeoner34 <atzarco37@gmail.com>
2023-05-02 19:28:40 +0200

    feat: added refresh icon on appbar

--- lib/presentation/screens/counter_screen.dart
+++ lib/presentation/screens/counter_screen.dart
@@ -1,25 +1,47 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
 
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int counterCount = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh_outlined),
+            onPressed: () {
+              setState(() {
+                counterCount = 0;
+              });
+            },
+            iconSize: 35,
+          ),
+        ],
         title: const Text('Counter Screen'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text('0', style: TextStyle(fontSize: 150)),
-            Text('Clicks', style: TextStyle(fontSize: 40))
+          children: [
+            Text('$counterCount', style: const TextStyle(fontSize: 150)),
+            Text(counterCount == 1 ? 'Click' : 'Clicks',
+                style: const TextStyle(fontSize: 40))
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            counterCount++;
+          });
+        },
         child: const Icon(Icons.plus_one),
       ),
     );
