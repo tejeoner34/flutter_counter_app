9356463608f0ac66a39cfa63800e15e57f6844e2
tejeoner34 <atzarco37@gmail.com>
2023-05-07 11:51:37 +0200

    feat: created CustomButton component

--- lib/presentation/screens/counter_screen.dart
+++ lib/presentation/screens/counter_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class CounterScreen extends StatefulWidget {
@@ -12,38 +14,88 @@ class _CounterScreenState extends State<CounterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.refresh_outlined),
-            onPressed: () {
-              setState(() {
-                counterCount = 0;
-              });
-            },
-            iconSize: 35,
-          ),
-        ],
-        title: const Text('Counter Screen'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('$counterCount', style: const TextStyle(fontSize: 150)),
-            Text(counterCount == 1 ? 'Click' : 'Clicks',
-                style: const TextStyle(fontSize: 40))
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.refresh_outlined),
+              onPressed: () {
+                setState(() {
+                  counterCount = 0;
+                });
+              },
+              iconSize: 35,
+            ),
           ],
+          title: const Text('Counter Screen'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            counterCount++;
-          });
-        },
-        child: const Icon(Icons.plus_one),
-      ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('$counterCount', style: const TextStyle(fontSize: 150)),
+              Text(counterCount == 1 ? 'Click' : 'Clicks',
+                  style: const TextStyle(fontSize: 40))
+            ],
+          ),
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            CustomButton(
+              icon: Icons.refresh_rounded,
+              onPressed: () {
+                setState(() {
+                  counterCount = 0;
+                });
+              },
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomButton(
+              icon: Icons.plus_one,
+              onPressed: () {
+                setState(() {
+                  counterCount++;
+                });
+              },
+            ),
+            const SizedBox(height: 15),
+            CustomButton(
+              icon: Icons.exposure_minus_1,
+              isStadiumBorder: true,
+              onPressed: () {
+                setState(() {
+                  if (counterCount > 0) {
+                    counterCount--;
+                  }
+                });
+              },
+            ),
+          ],
+        ));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final bool isStadiumBorder;
+  final VoidCallback? onPressed;
+
+  const CustomButton({
+    super.key,
+    required this.icon,
+    this.isStadiumBorder = false,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      enableFeedback: true,
+      onPressed: onPressed,
+      shape: isStadiumBorder ? const StadiumBorder() : null,
+      child: Icon(icon),
     );
   }
 }
